06b1afc99698a30fdbbd20ae9b949187be9b5b2a
Eduardo Barrantes <ebarrantes700937@ufide.ac.cr>
2022-08-02 14:02:12 -0600

    Update LugarFragment.kt

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -19,7 +19,7 @@ class LugarFragment : Fragment() {
     private lateinit var  lugarViewModel: LugarViewModel
 
     private var _binding: FragmentLugarBinding? = null
-    private val binding get() = _binding!!
+	 private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
